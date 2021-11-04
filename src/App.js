fca6dfba96ab87ea04a6c76a0857ca4757c9b7c1
saretoduncan <saretoduncan@gmail.com>
2021-11-04 10:55:18 +0300

    make search bar reusable

--- src/App.js
+++ src/App.js
@@ -2,10 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import CountButton from './components/counter/Countbutton';
 import Search from './components/searchBar/search';
+const teethProducts = [
+    "tooth paste",
+    "mouth wash",
+    "dental floss",
+    "mouth guard"
+    
+    ]
+const bikeProducts=[
+    "bike rack",
+    "mountain bike",
+    "soccer ball"
+]
 const App=()=> {
     return ( <
         div className = "App" >
-        <Search/>
+        <Search products={teethProducts}/>
+        <Search products={bikeProducts}/>
               <CountButton incrementBy={5} buttonBackgroundColor={"red"}/>
               <CountButton incrementBy={3} buttonBackgroundColor={"blue"}/>
               <CountButton incrementBy={2} buttonBackgroundColor={"green"}/>
--- src/components/searchBar/search.js
+++ src/components/searchBar/search.js
@@ -1,23 +1,17 @@
 import React, { useState } from "react";
 import "./search.css" 
-const products = [
-"tooth paste",
-"mouth wash",
-"dental floss",
-"mouth guard"
 
-]
-const Search=()=>{
+const Search=(props)=>{
     
     const [searchValue, setSearchValue]= useState("")
     const handleInputChanage=(event)=>{
         setSearchValue(event.target.value)
     }
-    const shouldDisplayButton= searchValue>0; 
+    const shouldDisplayButton= searchValue.length>0; 
     const handleClearClick=()=>{
             setSearchValue("")
     }
-    const filteredProducts= products.filter((products)=> products.includes(searchValue))
+    const filteredProducts= props.products.filter((products)=> products.includes(searchValue))
     return(
         <div>
             <input type="text" value={searchValue} onChange={handleInputChanage}/>
