fe5ed4330e11e4a853a5516101045c7640e65a13
saretoduncan <saretoduncan@gmail.com>
2021-11-04 10:39:29 +0300

    add filter method to search

--- src/components/searchBar/search.js
+++ src/components/searchBar/search.js
@@ -1,18 +1,32 @@
 import React, { useState } from "react";
 import "./search.css" 
+const products = [
+"tooth paste",
+"mouth wash",
+"dental floss",
+"mouth guard"
+
+]
 const Search=()=>{
+    
     const [searchValue, setSearchValue]= useState("")
     const handleInputChanage=(event)=>{
         setSearchValue(event.target.value)
     }
-    const shouldDisplayButton= searchValue>0;
+    const shouldDisplayButton= searchValue>0; 
     const handleClearClick=()=>{
             setSearchValue("")
     }
+    const filteredProducts= products.filter((products)=> products.includes(searchValue))
     return(
         <div>
             <input type="text" value={searchValue} onChange={handleInputChanage}/>
            {shouldDisplayButton && <button onClick={handleClearClick}> clear </button>}
+           <ul>
+               {filteredProducts.map((product)=>{
+                   return <li key={product}>{product}</li>
+               })}
+           </ul>
         </div>
     )
 }
